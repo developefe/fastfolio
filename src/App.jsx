a162ae77e3cf5f6cc80a37cea3920163b307dda2
Efe Işık <efemuhammedsk@gmail.com>
2022-10-18 17:44:09 +0300

    edited-fastfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
 import Templates from "./pages/Templates";
+import Landing from "./pages/Landing";
+import Header from "./components/Header/Header";
 
 function App() {
     return(
-        <div>
-            <Routes>
-                <Route path="templates" element={<Templates />} />
-            </Routes>
-        </div>
+        <>
+            <Header/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/templates" element={<Templates />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+    return(
+        <div>
+            landing
+            <Link to="/templates">Templates</Link>
+        </div>
+    )
+}
