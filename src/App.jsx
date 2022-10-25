d74977d2f12841e5a452262b0e759918eb1d54dc
Efe Işık <52710548+developefe@users.noreply.github.com>
2022-10-25 22:13:55 +0300

    including-themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
 import Templates from "./pages/Templates";
 import Landing from "./pages/home/Home";
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Theme1 from "./themes/theme-1/Theme1";
 import  "./index.scss";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Landing />} />
                     <Route path="/templates" element={<Templates />} />
+                    <Route path="/theme1" element={<Theme1 />} />
                 </Routes>
             </BrowserRouter>
             <Footer/>
